15a37e5ef1341d329ce02f0824daa3bf6e7ffe9a
unknown <alas@eyrd.ga>
2021-04-26 16:12:54 -0400

    1

---
+++ app/include/zmk/handlers.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <dt-bindings/zmk/keys.h>
+
+#include <zmk/keymap.h>
+#include <zmk/keys.h>
+
+void zmk_handle_key(struct zmk_key_event key_event);
